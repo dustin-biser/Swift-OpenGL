6b841fbd8c3a09ccd863a9ee055277b5b4e52c24
dustin-biser <dustinbiser@gmail.com>
2016-01-17 01:51:41 -0500

    glClear color working on iOS

---
+++ Swift-OpenGL/BridgingHeader.h
@@ -0,0 +1,15 @@
+//
+//  BridgingHeader.h
+//  Swift-OpenGL
+//
+//  Created by Dustin on 1/17/16.
+//  Copyright © 2016 none. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
+
+#endif /* BridgingHeader_h */
--- Swift-OpenGL/ViewController.swift
+++ Swift-OpenGL/ViewController.swift
@@ -6,20 +6,46 @@
 //  Copyright © 2016 none. All rights reserved.
 //
 
-import UIKit
+import GLKit
 
-class ViewController: UIViewController {
+class ViewController: GLKViewController {
 
+    //-----------------------------------------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // The GLKView associated with this GLKViewController will be loaded from the
+        // storyboard nib.
+        unowned let glkView = self.view as! GLKView
+        
+        // Create an OpenGL ES context and assign it to the GLKView.
+        glkView.context = EAGLContext(API:EAGLRenderingAPI.OpenGLES3)
+        
+        // Configure renderbuffers created by the view
+        glkView.drawableColorFormat = GLKViewDrawableColorFormat.RGBA8888
+        glkView.drawableDepthFormat = GLKViewDrawableDepthFormat.Format24
+        glkView.drawableStencilFormat = GLKViewDrawableStencilFormat.Format8
+        
+        // Enable multisampling
+        glkView.drawableMultisample = GLKViewDrawableMultisample.Multisample4X;
+        
+        glkView.delegate = self
     }
 
+    //-----------------------------------------------------------------------------------
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+
+    //-----------------------------------------------------------------------------------
+    override func glkView(view: GLKView, drawInRect rect: CGRect) {
+        glClearColor(0.7, 0.2, 0.2, 0.0)
+        glClear(GLbitfield(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT))
+        
+    }
+    
 }
 
