38e52d2313e2dcc485457d8cb1ab586ca528a0bc
dustin-biser <dustinbiser@gmail.com>
2016-01-17 21:49:33 -0500

    Added base OSX app and storyboard files

---
+++ Source/Classes/OSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swift-OpenGL-OSX
+//
+//  Created by Dustin on 1/17/16.
+//  Copyright © 2016 none. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
